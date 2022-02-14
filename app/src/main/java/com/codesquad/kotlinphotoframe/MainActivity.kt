36ea62b8c46f63228e8e174182aa5be3f2f0edba
sim <shim506@naver.com>
2022-02-15 00:18:12 +0900

    Logcat 활용 완료

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -2,10 +2,18 @@ package com.codesquad.kotlinphotoframe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate")
     }
 }
