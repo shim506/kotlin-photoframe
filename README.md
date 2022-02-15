d8d18d9d6937f61affb196e32cdf00d9afb891bf
sim <shim506@naver.com>
2022-02-15 14:35:26 +0900

    TextView 구현하기 완료

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-README.md 파일을 자신의 프로젝트에 대한 설명으로 변경한다.
-단계별로 미션을 해결하고 리뷰를 받고나면 README.md 파일에 주요 작업 내용(바뀐 화면 이미지, 핵심 기능 설명)과 완성 날짜시간을 기록한다.
-실행한 화면을 캡처해서 README.md 파일에 포함한다.
+README.md 파일을 자신의 프로젝트에 대한 설명으로 변경한다. 단계별로 미션을 해결하고 리뷰를 받고나면 README.md 파일에 주요 작업 내용(바뀐 화면 이미지, 핵심 기능
+설명)과 완성 날짜시간을 기록한다. 실행한 화면을 캡처해서 README.md 파일에 포함한다.
 
 # Step 1
 
+## 프로젝트 실행하기
+
 - [x] Logcat 활용하기
-![img.png](img.png)
+  ![img.png](img.png)
+
+## TextView 구현하기
+
+- [x] TextView 추가하기
+- [x] Design Editor 기능 활용해보기
+- [x] MainActivity에서 TextView에 대한 참조를 얻어 text를 변경한다.
+    - [x] findViewById 활용하기
+    - [x] onCreate 함수에서 text 변경하기
+    - [x] TextView 글자색 , 배경색 ,글자 크기 변경해보기
+
+![img_1.png](img_1.png)
+
+## Button 구현하기
+
+## 다른 Activity 로 이동 하기
 
--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.codesquad.kotlinphotoframe
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.annotation.Dimension
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        var tv_MyName : TextView = findViewById(R.id.tv_MyName)
+
+        tv_MyName.text = "Jay의 사진 액자"
+        tv_MyName.setTextSize(Dimension.SP , 40F)
+
+
+        // 회색
+        tv_MyName.setTextColor(Color.rgb(146,146,146))
+
+        // 하늘색
+        tv_MyName.setBackgroundColor(Color.parseColor("#89a5ea"))
+
+
         Log.d(TAG, "onCreate")
+        Log.d(TAG , tv_MyName.left.toString())
+
     }
 }
