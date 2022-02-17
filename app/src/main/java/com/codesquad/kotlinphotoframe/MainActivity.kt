cee17ec1cf116d00dc9d45a8e4c253b3e4170027
sim <shim506@naver.com>
2022-02-17 16:18:56 +0900

    FEAT : 버튼 클릭시 사진 변화 구현
    - 사진 로딩 하기 구현
    - 경로검사 함수 구현

--- app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MainActivity.kt
@@ -1,46 +1,39 @@
 package com.codesquad.kotlinphotoframe
 
-import android.content.Intent
-import android.content.res.Configuration
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Switch
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.contract.ActivityResultContracts.GetContent
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.annotation.Dimension
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.material.snackbar.Snackbar
-import java.util.*
+
 
 private const val TAG = "MainActivity"
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var constLayout: View
+    private lateinit var tvMyName: TextView
+    private lateinit var btnMyButton: Button
+    private lateinit var swDarkMode: Switch
+    private lateinit var ivMyImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
 
-        val constLayout = findViewById<View>(R.id.constLayout)
-        val myName: TextView = findViewById(R.id.tvMyName)
-        val myButton: Button = findViewById(R.id.btnMyButton)
-        val swDarkMode: Switch = findViewById(R.id.swDarkMode)
-        val myImage: ImageView = findViewById(R.id.ivMyImage)
+        constLayout = findViewById<View>(R.id.constLayout)
+        tvMyName = findViewById(R.id.tvMyName)
+        btnMyButton = findViewById(R.id.btnMyButton)
+        swDarkMode = findViewById(R.id.swDarkMode)
+        ivMyImage = findViewById(R.id.ivMyImage)
 
-        myName.text = "Jay의 사진 액자"
+        tvMyName.text = "Jay의 사진 액자"
 
         darkModeActivate(swDarkMode)
 
@@ -56,31 +49,31 @@ class MainActivity : AppCompatActivity() {
             getResult.launch(intent)
         }*/
 
-        // step 1. asset 폴더에서 파일 열기
-
-        MyImage("", assetManager)
-        val inputStream = assetManager.open("01.jpg")
-        val bitmap = BitmapFactory.decodeStream(inputStream)
+        imageChange()
 
 
-        // step 2. imageView에 표시
-        myImage.setImageBitmap(bitmap)
-        myImage.scaleType = ImageView.ScaleType.CENTER_CROP
+    }
 
+    private fun imageChange() {
+        btnMyButton.setOnClickListener {
 
-        getRandomImage()
+            // step 2. imageView에 표시
+            ivMyImage.setImageBitmap(getRandomImage())
+            ivMyImage.scaleType = ImageView.ScaleType.CENTER_CROP
+        }
+
     }
 
     private fun getRandomImage(): Bitmap {
         var randValStr = (1..22).random().toString()
-        val path = if (randValStr.length >= 2) "0${randValStr}.jpg" else "${randValStr}.jpg"
-        MyImage(path, resources.assets).getImage()
+        val path = if (randValStr.length < 2) "0${randValStr}.jpg" else "${randValStr}.jpg"
+
+        return MyImage(path, resources.assets).getImage()
     }
 
 
     private fun darkModeActivate(darkModeSwitch: Switch) {
         darkModeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             } else {
--- app/src/main/java/com/codesquad/kotlinphotoframe/MyImage.kt
+++ app/src/main/java/com/codesquad/kotlinphotoframe/MyImage.kt
@@ -3,20 +3,40 @@ package com.codesquad.kotlinphotoframe
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.Log
 import kotlin.coroutines.coroutineContext
 
+private const val TAG = "MyImage"
+
 class MyImage(private val filePath: String, private val assetManager: AssetManager) {
+
     var imageData: Bitmap? = null
 
     fun getImage(): Bitmap {
 
         if (imageData == null) {
             // step 1. asset 폴더에서 파일 열기
+            try {
+                if (!pathIsOk(filePath)) {
+                    throw WrongPathException(filePath)
+                }
+            } catch (e: WrongPathException) {
+                Log.d(TAG, "문제 경로 : $filePath")
+            }
+
+
             val inputStream = assetManager.open(filePath)
             val bitmap = BitmapFactory.decodeStream(inputStream)
             imageData = bitmap
         }
+
         return imageData as Bitmap
+    }
+
+    private fun pathIsOk(filePath: String): Boolean {
+        val rightPathList = mutableListOf<String>()
+        for (i in 1..22) if (i < 10) rightPathList.add("0${i}.jpg") else rightPathList.add("${i}.jpg")
+        return rightPathList.any { it == filePath }
     }
 
 }
